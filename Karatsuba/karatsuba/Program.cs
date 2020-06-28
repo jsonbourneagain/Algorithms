98712c314bfae4544913fa44090560f8a42a9e2d
Json Bourne <jsonbourneagain@outlook.com>
2020-06-28 18:52:48 +0530

    completed week 1 assignment

--- Karatsuba/karatsuba/Program.cs
+++ Karatsuba/karatsuba/Program.cs
@@ -1,50 +1,57 @@
 ﻿using System;
-namespace karatsuba
+using System.Numerics;
+
+namespace KaratsubaMultiplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int x = 1342;
-            int y = 5673;
+            BigInteger x = BigInteger.Parse("3141592653589793238462643383279502884197169399375105820974944592");
+
+            BigInteger y = BigInteger.Parse("2718281828459045235360287471352662497757247093699959574966967627");
+
+            //BigInteger x = BigInteger.Parse("3141592653589793");
+
+            //BigInteger y = BigInteger.Parse("2718281828459045");
+
             Console.WriteLine(RecIntMul(x, y));
+            Console.ReadKey();
         }
 
         // Input: two n-digit positive integers x and y
         // Output: the product x.y
         // Assumption: n is a power of 2.
 
-        static Int32 RecIntMul(int x, int y)
+        static BigInteger RecIntMul(BigInteger x, BigInteger y)
         {
-            // If any of the numbers are zero, return 0
-            if (x == 0 || y == 0)
-                return 0;
-
-            // Calculate the number of digits in x & y and take min() of that for computation
-            var n = Convert.ToInt32(Math.Min(Math.Floor(Math.Log10(x) + 1), Math.Floor(Math.Log10(y) + 1)));
-
-            var m = n / 2;
             // Base case for the recursion
-            if (n == 1)
+            if (x < 10 && y < 10)
                 return x * y;
 
             // Divide the given number in 2 halves, first and second
             else
             {
-                var a = Convert.ToInt32(Math.Floor(x / Math.Pow(10, m)));
-                var b = Convert.ToInt32(Math.Floor(x % Math.Pow(10, m)));
-                var c = Convert.ToInt32(Math.Floor(y / Math.Pow(10, m)));
-                var d = Convert.ToInt32(Math.Floor(y % Math.Pow(10, m)));
+                // Calculate the number of digits in x & y and take max() of that for computation
+                var n = (int)Math.Max(Math.Floor(BigInteger.Log10(x) + 1), Math.Floor(BigInteger.Log10(y) + 1));
 
-                Int32 p = a + b; Int32 q = c + d;
+                var m = (n + 1) / 2;
+
+                var a = (x / BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+                var b = (x % BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+                var c = (y / BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+                var d = (y % BigInteger.Parse(BigInteger.Pow(10, m).ToString()));
+
+                BigInteger p = a + b; BigInteger q = c + d;
 
                 var ac = RecIntMul(a, c);
                 var bd = RecIntMul(b, d);
                 var pq = RecIntMul(p, q);
+                var ad = RecIntMul(a, d);
+                var bc = RecIntMul(b, c);
+                //var adbc = pq - ac - bd;
 
-                var adbc = pq - ac - bd;
-
-                var result = Convert.ToInt32(Math.Pow(10, n) * ac + Math.Pow(10, n / 2) * adbc + bd);
+                var result = (BigInteger.Parse(BigInteger.Pow(10, n).ToString()) * ac) + (BigInteger.Parse(BigInteger.Pow(10, m).ToString()) * (ad + bc)) + bd;
 
                 return result;
             }
