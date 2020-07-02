4f722ae6c817498722e8347823c2e29a09082a3a
Json Bourne <jsonbourneagain@outlook.com>
2020-07-03 01:52:26 +0530

    Solved SplitInversion problem with the help of divide and conquer method in O(nlogn) time.

---
+++ SplitInversion/ArrayAndInversion.cs
@@ -0,0 +1,6 @@
+using System.Linq;
+public class ArrayAndInversion
+{
+    public int[] Arr { get; set; }
+    public int Inversions { get; set; }
+}
---
+++ SplitInversion/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace SplitInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[6] { 1, 3, 5, 2, 4, 6 };
+
+            Console.WriteLine(SortAndCountInversion(arr).Inversions);
+        }
+
+        public static ArrayAndInversion SortAndCountInversion(int[] A)
+        {
+            int length = A.Length;
+            int n = length / 2;
+
+            if (length == 0 || length == 1)
+                return new ArrayAndInversion { Arr = A, Inversions = 0 };
+
+            ArrayAndInversion left = SortAndCountInversion(A.Take(n).ToArray());
+            ArrayAndInversion right = SortAndCountInversion(A.Skip(n).ToArray());
+            ArrayAndInversion split = MergeAndCountSplitInversion(left, right);
+
+            return new ArrayAndInversion { Arr = split.Arr, Inversions = left.Inversions + right.Inversions + split.Inversions };
+
+            ArrayAndInversion MergeAndCountSplitInversion(ArrayAndInversion left, ArrayAndInversion right)
+            {
+                int i = 0, j = 0, splitInversions = 0;
+                int x= left.Arr.Length + right.Arr.Length;
+
+                int[] B = new int[x];
+
+                for (int k = 0; k < x; k++)
+                {
+                    if (i < left.Arr.Length && j < right.Arr.Length)
+                    {
+
+                        if (left.Arr[i] < right.Arr[j])
+                        {
+                            B[k] = left.Arr[i];
+                            i++;
+                        }
+                        else
+                        {
+                            B[k] = right.Arr[j];
+                            j++;
+                            splitInversions += (x/ 2 - i);
+                        }
+                    }
+                    else if (i < left.Arr.Length && j == right.Arr.Length)
+                    {
+                        B[k] = left.Arr[i];
+                        i++;
+                    }
+                    else if (i == left.Arr.Length && j < right.Arr.Length)
+                    {
+                        B[k] = right.Arr[j];
+                        j++;
+                    }
+                }
+                return new ArrayAndInversion { Arr = B, Inversions = splitInversions };
+            }
+        }
+    }
+}
