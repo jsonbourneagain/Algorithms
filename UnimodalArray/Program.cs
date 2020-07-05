08cd3ae4e4ae295124ccd3fdbbf7af6698a57f9b
Lavakesh Pandey <jsonbourneagain@outlook.com>
2020-07-05 16:46:59 +0530

    Find max of a unimodal array in log(n) time.

---
+++ UnimodalArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace UnimodalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 1, 3, 5, 50, 54, 79, 80, 90, 91, 25, 24, 23, 22, 20 };
+
+            Console.WriteLine(MaxOfUnimodalArray(A));
+        }
+
+        static int MaxOfUnimodalArray(int[] A)
+        {
+
+            if (A.Length == 1)
+                return A[0];
+            else
+            {
+                var n = A.Length;
+                var left = A.Take(n / 2);
+                var right = A.Skip(n / 2);
+
+                if (left.Last() > right.First())
+                    return MaxOfUnimodalArray(left.ToArray());
+                else
+                    return MaxOfUnimodalArray(right.ToArray());
+            }
+        }
+    }
+}
