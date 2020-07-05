43dbc191e3cdce423dd2fca670cba45cd625ee81
Lavakesh Pandey <jsonbourneagain@outlook.com>
2020-07-05 14:45:00 +0530

    Added a method to calculate a raised power b

---
+++ FastPower/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FastPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FastPower(3,4));
+        }
+
+        static int FastPower(int a, int b)
+        {
+            int c = default, answer = default;
+            if (b == 1)
+                return a;
+            else
+            {
+                c = a * a;
+                answer = FastPower(c, b / 2);
+            }
+            if (b % 2 == 0)
+                return answer;
+            else
+                return a * answer;
+        }
+    }
+}
